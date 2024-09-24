0fae467175a60d459abd540036e143a7fed80178
GBRmhe <mhe@consumertrack.com>
2024-09-24 17:09:10 -0400

    init

---
+++ src/main/java/io/github/michaelheuva/twenty_forty_eight/TwentyFortyEightApplication.java
@@ -0,0 +1,13 @@
+package io.github.michaelheuva.twenty_forty_eight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwentyFortyEightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwentyFortyEightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/michaelheuva/twenty_forty_eight/TwentyFortyEightApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.michaelheuva.twenty_forty_eight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwentyFortyEightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
